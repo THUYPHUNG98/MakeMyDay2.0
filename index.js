b3130c536c838028ad3b23c95376ab4ad20fbe0d
ThuyPhung <beastismylove365@gmail.com>
2019-11-15 23:04:22 +0700

    ok html

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        const unsetContainer = document.getElementById('unset_list');
-        const userId = user.uid;
-        let works = firebase.database().ref(userId + '/works');
-        works.on('value', function(snapshot) {
-            console.log(snapshot.val());
-            const worksList = snapshot.val();
-            for(let i = 0; i < worksList.length; i++) {
-                console.log(worksList[i]);
-                if(worksList[i].date == null|| worksList[i].date == undefined) {
-                    if(worksList[i].label == 0) {
-                        unsetContainer.insertAdjacentHTML('beforeend', `<li class="blue">${worksList[i].name}</li>`);
-                    } else if(worksList[i].label == 1) {
-                        unsetContainer.insertAdjacentHTML('beforeend', `<li class="yellow">${worksList[i].name}</li>`);
-                    } else if(worksList[i].label == 2) {
-                        unsetContainer.insertAdjacentHTML('beforeend', `<li class="green">${worksList[i].name}</li>`);
-                    } else {
-                        unsetContainer.insertAdjacentHTML('beforeend', `<li class="red">${worksList[i].name}</li>`);
-                    }
-                };
-                
-            }   
-        });
-
-    } else {
-        console.log('fail!');
-    }
-  });
-
 ////////////////////////////////////////////////////////////////////////////////
 // get full time - chỉ 1 lần
     var d = new Date();
@@ -200,8 +170,8 @@ function get_calendar1(firstDay_no, daysInMonth,innerDate1){
 const add_btn = document.getElementById('addBtn');
 var task_panel = document.getElementById('task_panel');
 add_btn.addEventListener('click', function(){
-    task_panel.style.display = "block";
-    task_panel.transform = "scale(1.1)"
+    task_panel.style.display = "flex";
+    task_panel.transform = "scale(1.1)";
 });
 
 // nút close của task panel
@@ -214,19 +184,146 @@ window.onclick = function(event) {
       task_panel.style.display = "none";
     }
 }
+//////// declare input fields
 const work_name = document.getElementById('input_name');
-const label_btn = document.getElementsByClassName('panel_btn');
-const label_container = document.getElementsByClassName('name1')[0];
-// const datesLabel = document.getElementsByClassName('panel_btn yellow');
-// const routineLabel = document.getElementsByClassName('panel_btn green');
-// const deadlineLabel = document.getElementsByClassName('panel_btn red');
-for(let i = 0; i < label_btn.length; i++) {
-    label_btn[i].addEventListener('click', function() {
-        label_btn[i].focus();
-    });
-}
-//unset button
+const work_date = document.getElementById('pick_time_date');
+const start_time = document.getElementById('start_time');
+const end_time = document.getElementById('end_time');
+const remind_btn = document.getElementById('remind_checkbox');
 const unset_btn = document.getElementById('unset_btn');
+const letMake_btn = document.getElementById('make_task');
+const label_container = document.getElementsByClassName('active');
+const header = document.getElementById('panel_cate');
+var label_btns = header.getElementsByClassName('panel_btn');
+for(var i = 0; i < label_btns.length; i++) {
+    label_btns[i].addEventListener('click', function() {
+        var current = document.getElementsByClassName('active');
+        console.log(current[0]);
+        
+        current[0].className = current[0].className.replace(' active','');
+        this.className += ' active';
+        console.log(this.className);
+        
+    });
+};
+// tương tác với database
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        const userId = user.uid;
+        let works = firebase.database().ref(userId + '/works');
+        works.on('value', function(snapshot) {
+            console.log(snapshot.val());
+            function checkFieldUnset() {
+                if(work_name.value == "") {
+                    alert('Enter your work name');
+                } else {
+                    console.log(work_name.value);
+                }
+                let label = 0;
+                if(label_container[0].textContent == 'Dates') {
+                    label = 1;
+                } else if(label_container[0].textContent == 'Routine') {
+                    label = 2;
+                } else if(label_container[0].textContent == 'Deadline') {
+                    label = 3;
+                } else {
+                    label = 0;
+                }
+                console.log(label);
+                return label;
+            };
+            function checkFieldSet() {
+                let dateArr = work_date.value.split('-');
+                let month = parseInt(dateArr[1]);
+                let day = parseInt(dateArr[2]);
+                date = dateArr[0] + month + day;
+                if(remind_btn.checked == true) {
+                    remind_btn.value = true;
+                }
+                console.log(date);
+                console.log(start_time.value);
+                console.log(end_time.value);
+                console.log(remind_btn.value);
+                return date;
+            };
+            const ref = firebase.database().ref(userId);
+            //console.log(snapshot.val().length);
+            if(snapshot.val() == null) { ///đăng nhập lần đầu chưa có works
+                //them first unset
+                unset_btn.addEventListener('click', function() {
+                    let label = checkFieldUnset();
+                    ref.child('works').set({
+                        0 : {
+                            label: label,
+                            name: work_name.value,
+                            },
+                        });
+                    });
+                //them first set
+                letMake_btn.addEventListener('click', function() {
+                    let label = checkFieldUnset();
+                    let date = checkFieldSet();
+                    ref.child('works').set({
+                        0 : {
+                            label: label,
+                            name: work_name.value,
+                            date: date, 
+                            startTime: start_time.value,
+                            endTime: end_time.value,
+                            remind: remind_btn.value,
+                            },
+                    });
+                });
+            } else {
+                const worksList = snapshot.val();
+                console.log(worksList.length);
+                unset_btn.addEventListener('click', function() {
+                    let label = checkFieldUnset();
+                    works.child(worksList.length).set({
+                        label: label,
+                        name: work_name.value,
+                    });
+                });
+                set_btn.addEventListener('click', function() {
+                    let label = checkFieldUnset();
+                    let date = checkFieldSet();
+                    works.child(worksList.length).set({
+                        label: label,
+                        name: work_name.value,
+                        date: date, 
+                        startTime: start_time.value,
+                        endTime: end_time.value,
+                        remind: remind_btn.value,
+                    });
+                });
+            };
+            // function unset_display() {
+                // const unsetContainer = document.getElementById('unset_list');
+                // works.on('value', function(snapshot){
+                //     const worksList = snapshot.val();
+                //     for(let i = 0; i < worksList.length; i++) {
+                //         console.log(worksList[i]);
+                //         if(worksList[i].date == null|| worksList[i].date == undefined) {
+                //             if(worksList[i].label == 0) {
+                //                 unsetContainer.insertAdjacentHTML('beforeend', `<li class="blue">${worksList[i].name}</li>`);
+                //             } else if(worksList[i].label == 1) {
+                //                 unsetContainer.insertAdjacentHTML('beforeend', `<li class="yellow">${worksList[i].name}</li>`);
+                //             } else if(worksList[i].label == 2) {
+                //                 unsetContainer.insertAdjacentHTML('beforeend', `<li class="green">${worksList[i].name}</li>`);
+                //             } else {
+                //                 unsetContainer.insertAdjacentHTML('beforeend', `<li class="red">${worksList[i].name}</li>`);
+                //             }
+                //         };
+                //     }; 
+                // });
+            // };
+        });
+    } else {
+        console.log('fail!');
+    }
+
+  });
+
 // set button
 let task_panel_content_holder = document.getElementById("task_panel_content_holder")
 let task_panel_content = document.getElementById("task_panel_content")
